8a10a42482c03f97e8465df141a4cfac42380d42
cbarange <cbarange.dev@gmail.com>
2019-09-26 14:07:19 +0200

    ajout de okhttp

--- app/src/main/java/com/example/filmflop/MainActivity.kt
+++ app/src/main/java/com/example/filmflop/MainActivity.kt
@@ -8,6 +8,51 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
+
+//import android.support.v7.app.AppCompatActivity
+
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import kotlin.properties.Delegates
+
+class OkHttpRequest(client: OkHttpClient) { // Small class to use okHttp quicker
+    private var client = OkHttpClient()
+
+    init {
+        this.client = client
+    }
+
+    fun GET(url: String, callback: Callback): Call {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        val call = client.newCall(request)
+        call.enqueue(callback)
+        return call
+    }
+
+    companion object {
+        val JSON = MediaType.parse("application/json; charset=utf-8")
+    }
+}
+
+fun JSONArray.toJSONObjectList(): List<JSONObject> { // Usage of extension
+    var buffer = emptyList<JSONObject>()
+    for(i in 0 until this.length()) {
+        buffer += this.getJSONObject(i)
+    }
+    return buffer
+}
+
+// =======================================================
+// ======================= MAIN CLASS ====================
+// =======================================================
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
